c5eccf919da5f876a186cd43d2bce9133c29b79b
whatwet <whatwet1100@163.com>
2026-09-23 19:35:54 +0800

    260923.2

---
+++ 72/test72.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+unordered_map<int, int> mp;
+using namespace std;
+
+inline int count_num(int x, int a)
+{
+    int res = 0;
+
+    return res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int m, n, res = 0;
+    cin >> m >> n;
+
+    for (int i = 1; i <= m; i++)
+    {
+        int temp = count_num(i, n);
+        res = res + temp;
+
+    }
+    cout << res << endl;
+    return 0;
+}
+
+//放弃了这段代码。。。
